d7bae8c6791203ccf22894441c843767fb562f5e
Mohamed BASSAOUD <mohamed.bassaoud@yahoo.fr>
2020-01-16 14:32:38 +0100

    TP1 Spring 16012020

---
+++ src/main/java/dev/App.java
@@ -0,0 +1,29 @@
+package dev;
+
+import dev.config.AppConfig;
+import dev.dao.PlatDaoFichier;
+import dev.dao.PlatDaoMemoire;
+import dev.ihm.Menu;
+import dev.service.PlatServiceVersion1;
+import dev.service.PlatServiceVersion2;
+
+import java.util.Scanner;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		// Création du contexte Spring
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-config.xml");
+		// récupération du bean Menu
+		Menu menu = context.getBean(Menu.class);
+		menu.afficher();
+		// fermeture du Scanner
+		context.getBean(Scanner.class).close();
+		// fermeture du contexte Spring
+		context.close();
+	}
+
+}
